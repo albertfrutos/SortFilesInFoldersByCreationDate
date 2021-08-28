17e5c9e583f85df612bcfaa5d61844294cf579da
albertfrutos <albertfg89@gmail.com>
2021-08-29 01:42:32 +0200

    First push, incomplete
    First push, incomplete

---
+++ SortFilesInFoldersByCreationDate/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace SortFilesInFoldersByCreationDate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string folderPath = @"/Users/albert/Pictures/iPhone Albert 19032020 al 27082020/3 sort";
+            string[] files = Directory.GetFiles(folderPath);
+
+            foreach (string file in files)
+            {
+                string creationDate = File.GetCreationTime(file).ToString("yyyy_MM_dd");
+
+                string targetDirectory = folderPath + "/" + creationDate;
+
+                if (!Directory.Exists(targetDirectory))
+                {
+                    Directory.CreateDirectory(targetDirectory);
+                }
+                File.Move(file, targetDirectory + "/" + Path.GetFileName(file));
+                
+            }
+        }
+    }
+}
